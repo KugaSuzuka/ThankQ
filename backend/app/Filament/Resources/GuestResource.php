ff54dc8fad2807c452f149e5f9d43cf48d4462f7
久賀　鈴香 <kuga.suzuka@suzukishouten.co.jp>
2025-04-24 23:52:16 +0900

    format修正

--- backend/app/Filament/Resources/GuestResource.php
+++ backend/app/Filament/Resources/GuestResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\GuestResource\Pages;
-use App\Filament\Resources\GuestResource\RelationManagers;
 use App\Models\Guest;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class GuestResource extends Resource
 {
