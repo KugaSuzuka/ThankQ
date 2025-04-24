4f8e9df2a6a0e713c7957115f78efffc0d40958d
久賀　鈴香 <kuga.suzuka@suzukishouten.co.jp>
2025-04-24 23:34:16 +0900

    filament構築、README更新

--- backend/database/migrations/2025_04_09_103347_create_guests_table.php
+++ backend/database/migrations/2025_04_09_103347_create_guests_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('guests', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
+            // どちら側のゲストなのか判定するためuser_idを付与する
             $table->foreignId('user_id')->comment('ユーザーID');
             $table->uuid('access_token')->unique()->comment('ゲスト専用アクセス用トークン');
             $table->text('thanks_message')->nullable()->comment('ゲストへの感謝メッセージ');
