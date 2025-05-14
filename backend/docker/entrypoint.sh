6a233052d12ca7763cc443e1d64095a967c13d48
久賀　鈴香 <kuga.suzuka@suzukishouten.co.jp>
2025-05-14 16:00:52 +0900

    Dockerfile修正

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -6,7 +6,7 @@ php artisan migrate --force
 
 echo "▶︎ Seeding initial data…"
 # Seeder が無い場合は行ごと削除して OK
-php artisan db:seed --class=InitialUserSeeder --force || true
+php artisan db:seed --class=UserSeeder --force || true
 
 echo "▶︎ Starting PHP built-in server"
 exec php -S 0.0.0.0:80 -t public
