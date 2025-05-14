e198d7317c501a61a65708cf0b5271e6a2ef6c8a
久賀　鈴香 <kuga.suzuka@suzukishouten.co.jp>
2025-05-15 01:12:13 +0900

    Dockerfileを修正

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -8,8 +8,5 @@ echo "▶︎ Clearing & rebuilding caches…"
 php artisan optimize:clear            # ② テーブルが出来た後にキャッシュ系を実行
 php artisan route:cache
 
-echo "▶︎ Seeding initial data…"
-php artisan db:seed --class=UserSeeder --force -v
-
 echo "▶︎ Starting PHP built‑in server"
 exec php -S 0.0.0.0:80 -t public      # フォアグラウンドで常駐（PID 1）
