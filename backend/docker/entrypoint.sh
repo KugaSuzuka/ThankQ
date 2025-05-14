812b989176a36fe3e786ea12c8c095f402a97bd1
久賀　鈴香 <kuga.suzuka@suzukishouten.co.jp>
2025-05-14 18:29:15 +0900

    UserSeeder.php修正

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -11,6 +11,8 @@ php artisan migrate --force
 echo "▶︎ Seeding initial data…"
 # Seeder が無い場合は行ごと削除して OK
 php artisan db:seed --class=UserSeeder --force || true
+php artisan vendor:publish --force --tag=livewire:assets
+
 
 echo "▶︎ Starting PHP built-in server"
 exec php -S 0.0.0.0:80 -t public
