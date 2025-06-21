8f21f5e37d9587f9c2b466c1c1660f1a74b1fc95
fujimmm331 <nyappy15th@gmail.com>
2025-06-21 18:57:29 +0900

    feat: チェキ風のコンポーネントを作成した

---
+++ frontend/src/components/messages/ InstaxCard/ InstaxCard.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import type { InstaxCardProps } from './Type.ts';
+import BaseCard from '@/components/Common/BaseCard/BaseCard.vue';
+import BaseHandWrittenText from '@/components/Common/BaseHandWrittenText/BaseHandWrittenText.vue';
+import BaseIcon from '@/components/Common/BaseIcon/BaseIcon.vue';
+
+const props = defineProps<InstaxCardProps>();
+const tapAnimation = ref(true);
+
+const width = computed(() => props.width ?? 202);
+const height = computed(() => props.height ?? 270);
+
+
+onMounted(() => {
+  setTimeout(() => {
+    tapAnimation.value = false;
+  }, 1000);
+})
+
+</script>
+
+<template>
+  <BaseCard
+    class="rounded-none bg-white pt-8 pr-5 pl-5 pb-12 photo-card shadow-md flex justify-center"
+  >
+    <img
+      class="w-full h-full"
+      :src
+    >
+    <BaseHandWrittenText class="text-xl absolute bottom-4 right-4 flex align-center">
+      Tap!!
+      <BaseIcon
+        class="tap-icon ml-1 mt-1"
+        :class="{
+          'tap-icon': tapAnimation,
+        }"
+        icon="touch_app"
+      />
+    </BaseHandWrittenText>
+  </BaseCard>
+</template>
+<style scoped>
+@keyframes tap-animation {
+    0%   { transform: scale(1); }
+    20%  { transform: scale(0.9); }
+    40%  { transform: scale(1.05); }
+    60%  { transform: scale(1); }
+    100% { transform: scale(1); }
+}
+
+.photo-card {
+  width: v-bind(width);
+  height: v-bind(height);
+}
+
+.tap-icon {
+  animation: tap-animation 1.2s ease-in-out infinite;
+  transform-origin: center;
+}
+</style>
---
+++ frontend/src/components/messages/ InstaxCard/Type.ts
@@ -0,0 +1,5 @@
+export interface  InstaxCardProps {
+  src: string;
+  width?: number;
+  height?: number;
+}
